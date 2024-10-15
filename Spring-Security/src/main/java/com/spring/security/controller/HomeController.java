1385558c10d5c0ac4d48993c23cdf825c22d2a04
MD. OMAR FARUQ <57213561+Md-Omar-Faruq@users.noreply.github.com>
2024-10-16 00:20:48 +0600

    Update Controller

--- Spring-Security/src/main/java/com/spring/security/controller/HomeController.java
+++ Spring-Security/src/main/java/com/spring/security/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.spring.security.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping("/")
-    public String Home(){
-        return "Welcom To Spring Security";
+    public String Home(HttpServletRequest request){
+        return "Welcom To Spring Security: "+ request.getSession().getId();
     }
 
 }
